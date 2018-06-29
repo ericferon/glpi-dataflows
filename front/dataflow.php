1b4f030a8369c661491e054aea26383b6fd29638
Eric Feron <eric_feron@carrefour.com>
2018-06-29 14:06:14 +0200

    License text updated

--- front/dataflow.php
+++ front/dataflow.php
@@ -1,23 +1,20 @@
 <?php
 /*
- * @version $Id: HEADER 15930 2011-10-30 15:47:55Z tsmr $
  -------------------------------------------------------------------------
- dataflows plugin for GLPI
- Copyright (C) 2009-2016 by the dataflows Development Team.
-
- https://github.com/InfotelGLPI/dataflows
+ Dataflows plugin for GLPI
+ Copyright C 2009-2018 by Eric Feron.
  -------------------------------------------------------------------------
 
  LICENSE
       
  This file is part of dataflows.
 
- dataflows is free software; you can redistribute it and/or modify
+ Dataflows is free software; you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation; either version 2 of the License, or
- (at your option) any later version.
+ at your option any later version.
 
- dataflows is distributed in the hope that it will be useful,
+ Dataflows is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
@@ -26,6 +23,7 @@
  along with dataflows. If not, see <http://www.gnu.org/licenses/>.
  --------------------------------------------------------------------------
  */
+
 
 include ('../../../inc/includes.php');
 
